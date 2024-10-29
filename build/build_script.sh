774a72fe48347643d60abb3d9a6d43705717e9af
DHEEMAN2912 <dheemandass2912@gmail.com>
2024-10-29 12:59:17 +0000

    Create build_script.sh

---
+++ build/build_script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Step 3: Build with Maven Wrapper
+echo "Building with Maven Wrapper..."
+# pwd
+# which java
+# echo "JAVA_HOME: $JAVA_HOME"
+# java -version
+# ls -la /usr/share/maven
+# mvn -version
+mvn clean package -DskipTests
+mv target/*.jar app.jar
+
+# Step 4: Generate artifact attestation
+echo "Generating artifact attestation..."
+npx actions-attest-build-provenance --subject-path ./app.jar
+
+# Step 6: Generate SBOM with Syft
+echo "Generating SBOM with Syft..."
+syft dir:. -o cyclonedx-json > sbom.json
+
+# Step 7: Upload SBOM
+echo "Uploading SBOM..."
+gh actions upload-artifact --name sbom --path sbom.json
+
+# Step 10: Generate SBOM attestation
+echo "Generating SBOM attestation..."
+npx actions-attest-sbom --subject-path ./sbom.json --sbom-path ./sbom.json
+
+# Step 11: Verify SBOM attestation
+echo "Verifying SBOM attestation..."
+gh attestation verify ./sbom.json --owner dheeman2912 --format=json
+
+# Step 12: Build Docker Image using Maven
+echo "Building Docker Image..."
+./mvnw clean spring-boot:build-image -Dmaven.test.skip=true
