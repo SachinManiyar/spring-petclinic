3f5daa4f5d12e7d248ede41101c5edd55ec36cf1
DHEEMAN2912 <dheemandass2912@gmail.com>
2024-11-04 11:12:29 +0000

    Update dockerfile_build.sh

--- build/dockerfile_build.sh
+++ build/dockerfile_build.sh
@@ -2,13 +2,14 @@
 
 # Define the build command
 # build_command=docker build -t ${IMAGE_NAME}:${IMAGE_VERSION} .
-build_command=docker build -t ghcr.io/dheeman2912/spring-petclinic:1.1 .
+# build_command=docker build -t ghcr.io/dheeman2912/spring-petclinic:1.1 .
+docker build -t ghcr.io/dheeman2912/spring-petclinic:1.1 . && { echo "Success: Image building succesfull"; } || { echo "Error: Image building failed"; exit 1; }
 
-# Run the Maven build and handle errors
-echo "Building Docker image using Maven..."
-if $build_command; then
-    echo "Docker image build completed successfully."
-else
-    echo "Error: Docker image build failed."
-    exit 1
-fi
+# # Run the Maven build and handle errors
+# echo "Building Docker image using Maven..."
+# if $build_command; then
+#     echo "Docker image build completed successfully."
+# else
+#     echo "Error: Docker image build failed."
+#     exit 1
+# fi
