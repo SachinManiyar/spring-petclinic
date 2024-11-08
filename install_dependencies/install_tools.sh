f988b3f4f35423fa804c36e782171445c4fc7de2
DHEEMAN2912 <dheemandass2912@gmail.com>
2024-11-08 10:36:08 +0000

    Update install_tools.sh

--- install_dependencies/install_tools.sh
+++ install_dependencies/install_tools.sh
@@ -49,4 +49,13 @@ curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s ht
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm kubectl  # Clean up after installation
 
+
+# Install Helm
+echo "Installing Helm..."
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | tee /usr/share/keyrings/helm.gpg > /dev/null
+apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | tee /etc/apt/sources.list.d/helm-stable-debian.list
+apt-get update
+apt-get install -y helm
+
 echo "All tools installed successfully."
